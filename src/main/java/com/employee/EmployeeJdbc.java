d6a5f65cbf1f8a95bf70efa04633c4865be4686c
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-12-11 02:49:57 +0530

    retriving values from a range of  date

--- src/main/java/com/employee/EmployeeJdbc.java
+++ src/main/java/com/employee/EmployeeJdbc.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class EmployeeJdbc {
 
-    //uc1 : for connection of database
+    // uc1 : for connection of database
     public Connection getConnectivityTest() throws SQLException {
         String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
         String username = "root";
@@ -27,14 +27,14 @@ public class EmployeeJdbc {
         return DriverManager.getConnection(jdbcUrl, username, password);
     }
 
-    //uc2 for retrival of data from database
+    // uc2 for retrival of data from database
     public List<Employee> getPayrollData() throws SQLException, PayrollServiceException {
         List<Employee> employees = new ArrayList<>();
 
         try (Connection connection = getConnectivityTest()) {
             String query = "SELECT * FROM EMPLOYEE_PAYROLL_DB";
             try (Statement statement = connection.createStatement();
-                 ResultSet resSet = statement.executeQuery(query)) {
+                    ResultSet resSet = statement.executeQuery(query)) {
                 while (resSet.next()) {
                     int id = resSet.getInt("EMP_ID");
                     String name = resSet.getString("NAME");
@@ -48,9 +48,8 @@ public class EmployeeJdbc {
                     String tax_pay = resSet.getString("TAXABLE_PAY");
                     String income_tax = resSet.getString("INCOME_TAX");
                     String net_pay = resSet.getString("NET_PAY");
-                    
 
-                    //create an EmployeePayroll object and add it to the list
+                    // create an EmployeePayroll object and add it to the list
                     Employee employee = new Employee(id, name, gender, salary, startDate, phone, department,
                             basic_pay, deductions, tax_pay, income_tax, net_pay);
                     employees.add(employee);
@@ -65,25 +64,63 @@ public class EmployeeJdbc {
 
     }
 
-    //uc3 - update values in db
-    public void updatePayrollSalary(String name, double salary) throws SQLException, PayrollServiceException{
-        try(Connection connection = getConnectivityTest()){
-            String updatequery = String.format("UPDATE EMPLOYEE_PAYROLL_DB SET SALARY = %.2f WHERE NAME = ?",salary);
-            try(PreparedStatement preparedStatement = connection.prepareStatement(updatequery)){
+    // uc3 &4 - update values in db
+    public void updatePayrollSalary(String name, double salary) throws SQLException, PayrollServiceException {
+        try (Connection connection = getConnectivityTest()) {
+            String updatequery = String.format("UPDATE EMPLOYEE_PAYROLL_DB SET SALARY = %.2f WHERE NAME = ?", salary);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(updatequery)) {
                 preparedStatement.setString(1, name);
                 int rows = preparedStatement.executeUpdate();
 
-                if(rows == 0){
+                if (rows == 0) {
                     System.out.println("Employee not found in the database!!!!");
                 }
             }
         }
     }
 
+    public List<Employee> getPayrollDataByDateRange(Date startDate, Date endDate)
+            throws SQLException, PayrollServiceException {
+        List<Employee> employees = new ArrayList<>();
+
+        try (Connection connection = getConnectivityTest()) {
+            String query = "SELECT * FROM EMPLOYEE_PAYROLL_DB WHERE START_DATE BETWEEN ? AND ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setDate(1, new java.sql.Date(startDate.getTime()));
+                preparedStatement.setDate(2, new java.sql.Date(endDate.getTime()));
+
+                try (ResultSet resSet = preparedStatement.executeQuery()) {
+                    while (resSet.next()) {
+                        int id = resSet.getInt("EMP_ID");
+                        String name = resSet.getString("NAME");
+                        double salary = resSet.getDouble("SALARY");
+                        Date joinDate = resSet.getDate("START_DATE");
+                        String phone = resSet.getString("PHONE");
+                        String gender = resSet.getString("GENDER");
+                        String department = resSet.getString("DEPARTMENT");
+                        String basic_pay = resSet.getString("BASIC_PAY");
+                        String deductions = resSet.getString("DEDUCTIONS");
+                        String tax_pay = resSet.getString("TAXABLE_PAY");
+                        String income_tax = resSet.getString("INCOME_TAX");
+                        String net_pay = resSet.getString("NET_PAY");
+
+                        Employee employee = new Employee(id, name, gender, salary, joinDate, phone, department,
+                                basic_pay, deductions, tax_pay, income_tax, net_pay);
+                        employees.add(employee);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new PayrollServiceException("Error retrieving employee payroll data: " + e.getMessage());
+        }
+
+        return employees;
+    }
+
 }
 
 class PayrollServiceException extends Exception {
-        public PayrollServiceException(String message) {
-            super(message);
-        }
+    public PayrollServiceException(String message) {
+        super(message);
     }
+}
--- src/main/java/com/employee/Main.java
+++ src/main/java/com/employee/Main.java
@@ -1,24 +1,24 @@
 package com.employee;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, PayrollServiceException {
+    public static void main(String[] args) throws SQLException, PayrollServiceException, java.text.ParseException {
         System.out.println("--------WELCOME TO EMPLOYEE PAYROLL SYSTEM------------");
 
         EmployeeJdbc jdbc = new EmployeeJdbc();
         jdbc.getConnectivityTest();
         List<Employee> employeePayrolls = jdbc.getPayrollData();
 
-        System.out.println("-----------THE DATABASE----------------");
-
-        // Process the retrieved employee payroll data
+        System.out.println("-----------THE DATABASE----------------\n");
         for (Employee employeePayroll : employeePayrolls) {
             System.out.println(employeePayroll);
         }
 
-        System.out.println("-----------THE UPDATED DATABASE----------------");
+        System.out.println("-----------THE UPDATED DATABASE----------------\n");
         try {
             jdbc.updatePayrollSalary("Terissa", 3000000.00);
 
@@ -28,6 +28,28 @@ public class Main {
                 System.out.println(employee);
             }
         } catch (SQLException | PayrollServiceException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            java.util.Date startDate = dateFormat.parse("2023-03-31");
+            java.util.Date endDate = dateFormat.parse("2023-04-31");
+
+            // Convert java.util.Date to java.sql.Date
+            java.sql.Date sqlStartDate = java.sql.Date.valueOf(dateFormat.format(startDate));
+            java.sql.Date sqlEndDate = java.sql.Date.valueOf(dateFormat.format(endDate));
+
+            // Retrieve employees in the specified date range
+            List<Employee> employeesInDateRange = jdbc.getPayrollDataByDateRange(sqlStartDate, sqlEndDate);
+            System.out.println("---------------------EMPLOYEEE FROM " + sqlStartDate + " TO " + sqlEndDate
+                    + "----------------------\n");
+            // Process the list of employees
+            for (Employee employee : employeesInDateRange) {
+                System.out.println(employee);
+            }
+        } catch (PayrollServiceException | SQLException | java.text.ParseException e) {
             e.printStackTrace();
         }
 
